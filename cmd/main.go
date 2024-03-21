db53ec55fa06d4b4239aaaf50c3d194d05805f6d
satori <emogycs@gmail.com>
2024-03-21 21:32:00 +0300

    added database with new logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 	todo "todo-app"
 	"todo-app/pkg/handler"
@@ -14,12 +14,13 @@ import (
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s", err.Error())
+		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
 
 	db, err := mysql.NewMySqlDB(&mysql.Config{
@@ -30,7 +31,7 @@ func main() {
 		DBName:   viper.GetString("db.dbname"),
 	})
 	if err != nil {
-		log.Fatalf("error occured while initializating database: %s", err.Error())
+		logrus.Fatalf("error occured while initializating database: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -39,7 +40,7 @@ func main() {
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRouter()); err != nil {
-		log.Fatalf("error occured while running server: %s", err.Error())
+		logrus.Fatalf("error occured while running server: %s", err.Error())
 	}
 }
 
