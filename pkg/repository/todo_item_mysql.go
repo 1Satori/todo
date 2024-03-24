c67f96a7e451242f6015c6f8f54a1cbaf04afe09
satori <emogycs@gmail.com>
2024-03-24 04:15:42 +0300

    added items

---
+++ pkg/repository/todo_item_mysql.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	todo "todo-app"
+)
+
+type TodoItemMysql struct {
+	db *sqlx.DB
+}
+
+func NewTodoItemMysql(db *sqlx.DB) *TodoItemMysql {
+	return &TodoItemMysql{db: db}
+}
+
+func (r *TodoItemMysql) Create(listId int, item todo.TodoItem) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	createItemQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES (?, ?)", todoItemsTable)
+	res, err := tx.Exec(createItemQuery, item.Title, item.Description)
+	if err != nil {
+		return 0, err
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	createListItemsQuery := fmt.Sprintf("INSERT INTO %s (list_id, item_id) values (? ?)", listsItemsTable)
+	_, err = tx.Exec(createListItemsQuery, listId, lastID)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return int(lastID), tx.Commit()
+}
+
+func (r *TodoItemMysql) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	var items []todo.TodoItem
+
+	query := fmt.Sprintf("SELECT * FROM %s ti INNER JOIN %s li ON li.item_id = ti.id INNER JOIN %s ul"+
+		" ON ul.list_id = li.list_id WHERE li.list_id = ? AND ul.user_id = ?",
+		todoItemsTable, listsItemsTable, usersListsTable)
+
+	if err := r.db.Select(&items, query, listId, userId); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
---
+++ pkg/service/todo_item.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	todo "todo-app"
+	"todo-app/pkg/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItem(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo: repo, listRepo: listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	return s.repo.GetAll(userId, listId)
+}
